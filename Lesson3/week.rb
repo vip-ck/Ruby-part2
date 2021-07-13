b9a328d2a21cc63a432e1d9bd57040e8aa10ee82
AgentSmith <vip1313@gmail.com>
2021-07-13 18:48:53 +0300

    create week

---
+++ Lesson3/week.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'date'
+class Date
+  def week(number)
+    i = 1
+    while i <= 7
+      yield Date.commercial(god, number, i).strftime('%d.%m.%Y')
+      i += 1
+    end
+  end
+end
+Date.new(2021).week(18) { |x| puts x }
