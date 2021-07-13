65b123afb2571855e4826f67936cc55de6f458b8
AgentSmith <vip1313@gmail.com>
2021-07-13 18:49:50 +0300

    fix

--- Lesson3/week.rb
+++ Lesson3/week.rb
@@ -5,7 +5,7 @@ class Date
   def week(number)
     i = 1
     while i <= 7
-      yield Date.commercial(god, number, i).strftime('%d.%m.%Y')
+      yield Date.commercial(year, number, i).strftime('%d.%m.%Y')
       i += 1
     end
   end
