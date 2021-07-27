f4ab5bbe297ff6f8f4aabb414114d7fc22b88400
AgentSmith <vip1313@gmail.com>
2021-07-28 00:28:51 +0300

    fix rubocop

--- Lesson8/4.rb
+++ Lesson8/4.rb
@@ -1,15 +1,23 @@
-class Animal 
+# frozen_string_literal: true
+
+class Animal
 end
+
 class Xordovie < Animal
 end
+
 class Mammalia < Xordovie
 end
+
 class Primat < Mammalia
 end
+
 class Gominid < Primat
 end
+
 class Men < Gominid
 end
+
 class Homosapiens < Men
 end
 petya = Homosapiens.new
@@ -20,3 +28,10 @@ dolphin = Mammalia.new
 deinonychus = Xordovie.new
 worm = Animal.new
 
+puts petya
+puts avstralopitek
+puts gorila
+puts king_julian
+puts dolphin
+puts deinonychus
+puts worm
